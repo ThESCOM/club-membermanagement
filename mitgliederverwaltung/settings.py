4495c472c4dfd8b7175d5dec0dfe18ccf7e2caec
Max Oberberger <max@oberbergers.de>
2016-06-01 21:39:14 +0200

    configure authentication backend of userAccounts

--- mitgliederverwaltung/settings.py
+++ mitgliederverwaltung/settings.py
@@ -87,6 +87,12 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'userAccounts.AdminUser'
+
+AUTHENTICATION_BACKENDS = ( 'userAccounts.backends.EmailAuthBackend', )
+
+LOGIN_REDIRECT_URL = '/'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
