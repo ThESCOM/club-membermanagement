44f45345b6b0c174715754387ca50b262b3e44d4
Max Oberberger <max@oberbergers.de>
2016-06-01 21:40:06 +0200

    add urls to mitgliederverwaltung

--- mitgliederverwaltung/urls.py
+++ mitgliederverwaltung/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
+import mitglieder.views
+import userAccounts.views
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/', userAccounts.views.login, name='login'),
+    url(r'^login$', userAccounts.views.login, name='login'),
+    url(r'^logout/', userAccounts.views.logout),
+    url(r'^user/', include('userAccounts.urls', namespace='user')),
+    url(r'^mitglieder/', include('mitglieder.urls', namespace='mitglieder')),
+    url(r'^$', mitglieder.views.main)
 ]
